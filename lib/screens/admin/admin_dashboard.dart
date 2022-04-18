5804658ac165e781b3192322d63ddd41730ca6fb
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-04-18 23:10:24 +0530

    updated dashboard

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -24,120 +24,144 @@ class _AdminDashboardState extends State<AdminDashboard> {
     return Scaffold(
       key: scaffoldKey,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/user-settings');
-                        // Authentication.logoutUser(context);
-                      },
-                      child: const Icon(
-                        Icons.menu,
-                        color: Colors.black,
-                      ),
-                    ),
-                    // const Icon(IconData(0xe3b3, fontFamily: 'MaterialIcons'))
-                  ],
-                ),
-              ),
-              Stack(
-                alignment:
-                    const AlignmentDirectional(-0.050000000000000044, 1.0),
-                children: [
-                  Align(
-                    alignment: const AlignmentDirectional(0, 0),
-                    child: Image.asset(
-                      'assets/red_ribbon.png',
-                      width: MediaQuery.of(context).size.width - 40.0,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Align(
-                    alignment: const AlignmentDirectional(0.0, 0.56),
-                    child: ProfilePic(
-                      //TODO: Add URL here for Profile Picture
-                      picURL: user.photoURL,
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15.0),
-                child: Center(
-                  child: Text(
-                    'Hi, ${user.displayName}',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 30),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    ContentText(text: "Members", value: "10"),
-                    ContentText(text: "Mess Cut", value: "5"),
-                    ContentText(text: "Guest", value: "0"),
-                  ],
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      DashboardCard(
-                          cardIcon: Icons.menu_book_sharp,
-                          cardName: "Menu",
-                          cardColor: const Color(0xFF99D5F3),
-                          cardAction: () {
-                            Navigator.pushNamed(context, '/mess-menu');
-                          }),
-                      DashboardCard(
-                        cardIcon: Icons.payment,
-                        cardName: "Statement",
-                        cardColor: const Color(0xFFFDCE84),
-                        cardAction: () {
-                          Navigator.pushNamed(context, '/admin-statement');
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/user-settings');
+                          // Authentication.logoutUser(context);
                         },
+                        child: const Icon(
+                          Icons.menu,
+                          color: Colors.black,
+                        ),
                       ),
+                      // const Icon(IconData(0xe3b3, fontFamily: 'MaterialIcons'))
                     ],
                   ),
-                  const SizedBox(
-                    height: 22.0,
+                ),
+                Stack(
+                  alignment:
+                      const AlignmentDirectional(-0.050000000000000044, 1.0),
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, 0),
+                      child: Image.asset(
+                        'assets/red_ribbon.png',
+                        width: MediaQuery.of(context).size.width - 40.0,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Align(
+                      alignment: const AlignmentDirectional(0.0, 0.56),
+                      child: ProfilePic(
+                        //TODO: Add URL here for Profile Picture
+                        picURL: user.photoURL,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  child: Center(
+                    child: Text(
+                      'Hi, ${user.displayName}',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  Row(
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 30),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: const [
-                      DashboardCard(
-                          cardIcon: Icons.payment,
-                          cardName: "Mess Details",
-                          cardColor: Color(0xFFFDCE84)),
-                      DashboardCard(
-                          cardIcon: Icons.payment,
-                          cardName: "Register",
-                          cardColor: Color(0xFF99D5F3))
+                      ContentText(text: "Members", value: "10"),
+                      ContentText(text: "Mess Cut", value: "5"),
+                      ContentText(text: "Guest", value: "0"),
                     ],
                   ),
-                ],
-              ),
-            ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DashboardCard(
+                            cardIcon: Icons.menu_book_sharp,
+                            cardName: "Menu",
+                            cardColor: const Color(0xFF99D5F3),
+                            cardAction: () {
+                              Navigator.pushNamed(context, '/mess-menu');
+                            }),
+                        DashboardCard(
+                          cardIcon: Icons.payment,
+                          cardName: "Statement",
+                          cardColor: const Color(0xFFFDCE84),
+                          cardAction: () {
+                            Navigator.pushNamed(context, '/admin-statement');
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 22.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        DashboardCard(
+                            cardIcon: Icons.payment,
+                            cardName: "Mess Details",
+                            cardColor: Color(0xFFFDCE84)),
+                        DashboardCard(
+                            cardIcon: Icons.payment,
+                            cardName: "Attendance",
+                            cardColor: Color(0xFF99D5F3))
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 22.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DashboardCard(
+                            cardIcon: Icons.payment,
+                            cardName: "Members",
+                            cardColor: const Color(0xFFFDCE84),
+                            cardAction: () {
+                              Navigator.pushNamed(context, '/mess-members');
+                            }),
+                        DashboardCard(
+                            cardIcon: Icons.payment,
+                            cardName: "View Feedbacks",
+                            cardColor: const Color(0xFF99D5F3),
+                            cardAction: () {
+                              Navigator.pushNamed(context, '/mess-members');
+                            }),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/dashboard_model.dart
+++ lib/screens/dashboard_model.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class DashboardCard extends StatelessWidget {
-  const DashboardCard({
-    Key? key,
-    required this.cardIcon,
-    required this.cardName,
-    required this.cardColor,
-    this.cardAction
-  }) : super(key: key);
+  const DashboardCard(
+      {Key? key,
+      required this.cardIcon,
+      required this.cardName,
+      required this.cardColor,
+      this.cardAction})
+      : super(key: key);
 
   final IconData cardIcon;
   final String cardName;
@@ -18,64 +18,65 @@ class DashboardCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Container(
-      width: 110,
-      height: 130,
-      decoration: BoxDecoration(
-        color: cardColor,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Stack(
-            children: [
-              Align(
-                alignment: const AlignmentDirectional(0, 0),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFEEEEEE),
-                      borderRadius: BorderRadius.circular(7.5),
-                    ),
-                    child: Icon(
-                      cardIcon,
-                      color: const Color(0xFF1F688B),
-                      size: 40,
+        width: 110,
+        height: 130,
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              children: [
+                Align(
+                  alignment: const AlignmentDirectional(0, 0),
+                  child: Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFEEEEEE),
+                        borderRadius: BorderRadius.circular(7.5),
+                      ),
+                      child: Icon(
+                        cardIcon,
+                        color: const Color(0xFF1F688B),
+                        size: 40,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Text(
-            cardName,
-            style: const TextStyle(
-              fontSize: 16,
-              color: Color(0xFF535353),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 10, 10),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Icon(
-                  Icons.arrow_forward_ios_rounded,
-                  color: Color(0xFF1F688B),
-                  size: 24,
-                ),
               ],
             ),
-          ),
-        ],
+            Text(
+              cardName,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Color(0xFF535353),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 10, 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Color(0xFF1F688B),
+                    size: 24,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
       onTap: () => cardAction!(),
     );
   }
