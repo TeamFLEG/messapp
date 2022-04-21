506d92682ec05da96335b6aadbb1335feaae6ea4
sharun <sharunpublic@gmai.com>
2022-04-21 19:53:37 +0530

    Update join mess

--- lib/screens/join_mess.dart
+++ lib/screens/join_mess.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:messapp/models/mess.dart';
 import 'package:messapp/widgets/mess_card.dart';
+import 'package:messapp/widgets/primary_button.dart';
 // import 'package:messapp/widgets/snack_bar_message.dart';
 
 class JoinMess extends StatefulWidget {
@@ -15,76 +16,75 @@ class _JoinMessState extends State<JoinMess> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Form(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    const Text(
-                      "Join Mess",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 30.0,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 80.0,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextFormField(
-                      // controller: _emailController,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.search,
-                            color: Color.fromRGBO(53, 53, 53, 1.0)),
-                        hintText: "Join By Mess Name or Mess ID",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
+                      const Text(
+                        "Join Mess",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 30.0,
                         ),
-                        filled: true,
-                        fillColor: const Color.fromRGBO(239, 239, 239, 1.0),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                  ],
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        // controller: _emailController,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.search,
+                              color: Color.fromRGBO(53, 53, 53, 1.0)),
+                          hintText: "Join By Mess Name or Mess ID",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: const Color.fromRGBO(239, 239, 239, 1.0),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(32.0),
-              child: MessCard(
-                  cardTitle: "Sagar Hostel for Boys", cardSubtitle: "CUSAT"),
-            ),
-            // StreamBuilder<List<Mess>>(
-            //   stream: getMessList(),
-            //   builder: (context, snapshot) {
-            //     print(snapshot);
-            //     if (snapshot.hasError) {
-            //       return Text(
-            //           "Something went wrong. Error message : ${snapshot.error}");
-            //     } else if (snapshot.hasData) {
-            //       final messData = snapshot.data!;
-            //       print(messData);
-            //       return ListView(
-            //         children: messData.map(buildMessCards).toList(),
-            //       );
-            //     } else {
-            //       return const Center(
-            //         child: CircularProgressIndicator(),
-            //       );
-            //     }
-            //   },
-            // ),
-          ],
+              FutureBuilder<QuerySnapshot>(
+                future: FirebaseFirestore.instance.collection('mess').get(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      children: snapshot.data!.docs.map((doc) {
+                        return MessCard(
+                          cardTitle: doc['messName'],
+                          cardSubtitle: doc['location'],
+                        );
+                      }).toList(),
+                    );
+                  } else {
+                    // or your loading widget here
+                    return Container();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
