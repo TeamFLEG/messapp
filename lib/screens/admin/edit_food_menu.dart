a48da0f43c8ddcad0bef35b64bb8f6d8dedc2b74
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-04-24 13:01:42 +0530

    updated food menu section

--- lib/screens/admin/edit_food_menu.dart
+++ lib/screens/admin/edit_food_menu.dart
@@ -11,14 +11,42 @@ class EditFoodMenu extends StatefulWidget {
 }
 
 class _EditFoodMenuState extends State<EditFoodMenu> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late TextEditingController _sunBreakfastController;
+
+  @override
+  void initState() {
+    super.initState();
+    _sunBreakfastController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(head: "Edit Food Menu"),
-      body: Column(
-        children: [
-
-        ]
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                controller: _sunBreakfastController,
+                enableSuggestions: true,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  labelText: "Breakfast",
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.text,
+              ),
+            ],
+          ),
+        ),
       ),
 
     );
